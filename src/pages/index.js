302bca3d5f813a752b77d38181c60c99ea8aefcb
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2023-10-07 22:25:12 +0900

    update: ちょっと変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/blog">
-            ブログを読んでみる
+            puts "ブログを読んでみる"
           </Link>
         </div>
       </div>
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
